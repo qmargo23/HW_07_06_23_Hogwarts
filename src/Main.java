6dda45630097c4e92d32b15ce6cc0d293a9fe471
qmargo23 <elen.asplund@gmail.com>
2023-06-07 07:39:32 +0300

    2.2 ООП часть 1. Инкапсуляция и наследование

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("2.2 ООП часть 1. Инкапсуляция и наследование");
     }
 }
